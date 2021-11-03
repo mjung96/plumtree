d45406afcbb02f334f4b659233c7f9f7aefc2a48
Michael Jung <michaeljung1996@gmail.com>
2021-11-03 18:31:08 -0400

    CoachingPage
    only has 3 dropdowns with dummy data, have to add functionality for the data in dropdowns to be taken from the jsonserver with apis.

--- src/pages/CoachingPage.js
+++ src/pages/CoachingPage.js
@@ -1,19 +1,123 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import { Button, Grid, Card, Box } from "@material-ui/core";
 
 export default function CoachingPage() {
-    // useEffect(() => {
-    //     fetch('http://localhost:3001/years/2019/')
-    //         .then(res => {
-    //             return res.json();
-    //         })
-    //         .then(data => {
-    //             console.log(data);
-    //             console.log(data.weeks[1].drivers)
-    //         })
-    // }, []);
+    const [driver, setDriver] = useState('');
+    const [year, setYear] = useState('');
+    const [week, setWeek] = useState('');
+    const [canClick, setCanClick] = useState(true);
+
+    const selectDriver = (event) => {
+        setDriver(event.target.value);
+    };
+
+    const selectYear = (event) => {
+        setYear(event.target.value);
+    };
+
+    const selectWeek = (event) => {
+        setWeek(event.target.value);
+    };
+
+    const showData = () => {
+        setDriver('');
+        setYear('');
+        setWeek('');
+        setCanClick(true);
+    }
+
+    useEffect(() => {
+        if (driver !== '' && year !== '' && week !== '') {
+            setCanClick(false);
+        }
+    }, [driver, year, week])
+
     return (
-        <>
-        <h1>Coaching</h1>
-        </>
+        <div>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-standard-label">Year</InputLabel>
+                <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={year}
+                onChange={selectYear}
+                label="Year"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={"2019"}>2019</MenuItem>
+                <MenuItem value={"2020"}>2020</MenuItem>
+                <MenuItem value={"2021"}>2021</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-standard-label">Week</InputLabel>
+                <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={week}
+                onChange={selectWeek}
+                label="Week"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={"1"}>1</MenuItem>
+                <MenuItem value={"2"}>2</MenuItem>
+                <MenuItem value={"3"}>3</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 240 }}>
+                <InputLabel id="demo-simple-select-standard-label">Driver</InputLabel>
+                <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={driver}
+                onChange={selectDriver}
+                label="Driver"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={"Driver1"}>Driver1</MenuItem>
+                <MenuItem value={"Driver2"}>Driver2</MenuItem>
+                <MenuItem value={"Driver3"}>Driver3</MenuItem>
+                </Select>
+            </FormControl>
+
+            <br/>
+
+            {canClick ? 
+                <Button 
+                disabled={true} 
+                variant="contained" 
+                style={{ marginLeft: 70, backgroundColor: '#79719880', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
+                disableElevation
+                >
+                    Submit
+                </Button>
+
+                :
+
+                <Button 
+                disabled={false} 
+                variant="contained" 
+                style={{ marginLeft: 70, backgroundColor: '#797198', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
+                disableElevation 
+                component="label"
+                onClick={showData}
+                >
+                    Submit
+                </Button>
+            }
+
+      </div>
     )
 }
