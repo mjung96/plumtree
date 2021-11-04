fc242aae4d37ccfe4597e27fbfdc316a51a06cd8
Michael Jung <michaeljung1996@gmail.com>
2021-11-04 10:52:41 -0400

    "finished" upload page

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
+import Fade from '@mui/material/Fade';
+import { FileUpload } from '@mui/icons-material';
 
 export default function UploadPage() {
-    const [fileName, setFileName] = useState("");
+    const [fileName, setFileName] = useState("Select File");
     const [canClick, setCanClick] = useState(true);
     const [loading, setLoading] = useState(false);
     const [uploadComplete, setUploadComplete] = useState(false);
@@ -27,9 +29,13 @@ export default function UploadPage() {
     const resetUploadPage = () => {
         setLoading(true);
         setCanClick(true);
-        setFileName("");
+        setFileName("Select File");
 
     }
+
+    // const uploadFile = () => {
+    //     console.log("hello");
+    // }
 
     const readUploadFile = (e) => {
         var file = e.target.value;
@@ -44,37 +50,35 @@ export default function UploadPage() {
 
     return (
         <>
-            <input 
-                type="file"
-                accept=".pdf"
-                onChange={(e) => readUploadFile(e)}
-            />
-
-            <TextField label="Select File" variant="standard" style={{ marginTop: '5%',marginLeft: '38%',width: '344px', height: '40px'}} value={fileName}>
-                
-            </TextField>
+        <Grid container spacing={0} alignItems="center" justifyContent="center" direction="column">
+            <label htmlFor="contained-button-file">
+                <input accept=".pdf" id="contained-button-file" multiple type="file" hidden onChange={(e) => readUploadFile(e)}/>
+                <Button variant="contained" component="span" style={{ marginTop: '25%', width: '344px', height: '40px'}}>
+                    {fileName}
+                </Button>
+            </label>
 
             <br/>
 
             {canClick ? 
                 <Button 
-                disabled={true} 
-                variant="contained" 
-                style={{ marginLeft: '38%', backgroundColor: '#79719880', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
-                disableElevation
-                >
-                    Submit
-                </Button>
+                    disabled={true} 
+                    variant="contained" 
+                    style={{ backgroundColor: '#79719880', color: 'white', width: '344px', height: '44px' }} 
+                    disableElevation
+                    >
+                        Submit
+                    </Button>
 
                 :
 
                 <Button 
-                disabled={false} 
-                variant="contained" 
-                style={{ marginLeft: '38%', backgroundColor: '#797198', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
-                disableElevation 
-                component="label"
-                onClick={resetUploadPage}
+                    disabled={false} 
+                    variant="contained" 
+                    style={{ backgroundColor: '#797198', color: 'white', width: '344px', height: '44px' }} 
+                    disableElevation 
+                    component="label"
+                    onClick={resetUploadPage}
                 >
                     Submit
                 </Button>
@@ -83,15 +87,89 @@ export default function UploadPage() {
             <br/>
 
             {loading ? 
-                <CircularProgress style={{ color: '#797198', marginTop: 50, marginLeft: '43%', width: '150px', height: '150px'}}/>
+                <Fade in={loading} out={!loading}><CircularProgress style={{ color: '#797198', marginTop: 50, width: '150px', height: '150px'}}/></Fade>
                 :
                 null
             }
             {uploadComplete ?
-                <h2 style={{ color: '#797198', marginTop: 50, marginLeft: '40%' }}>Upload Complete</h2>
+                <Fade in={uploadComplete} out={!uploadComplete}><h2 style={{ color: '#797198', marginTop: 50 }}>Upload Complete</h2></Fade>
                 :
                 null
             }
+        </Grid>
+
+
+            {/* <TextField
+					onClick={onButtonClick}
+					label={file?.name || 'Select File'}
+					fullWidth={matches ? true : false}
+					style={{ minWidth: (matches ? 0 : 500) }}
+					disabled
+					className={(file === '' ? classes.fileRoot : classes.afterRoot)}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<InsertDriveFileOutlinedIcon />
+							</InputAdornment>
+						)
+					}}
+				/>
+				<input type='file' id='file' ref={inputFile} style={{ display: 'none' }} onChange={(e) => { setFile(e.target.files.length === 0 ? file : e.target.files[0]) }} /> */}
+
+            {/* <label htmlFor="contained-button-file">
+                <input accept=".pdf" id="contained-button-file" multiple type="file" hidden onChange={(e) => readUploadFile(e)}/>
+                <Button variant="contained" component="span" style={{ marginTop: '5%', marginLeft: '38%', width: '344px', height: '40px'}}>
+                    {fileName}
+                </Button>
+            </label> */}
+
+            {/* <input 
+                type="file"
+                hidden
+                accept=".pdf"
+                onChange={(e) => readUploadFile(e)}
+            />
+
+            <TextField onClick={uploadFile} label="Select File" variant="standard" style={{ marginTop: '5%',marginLeft: '38%',width: '344px', height: '40px'}} value={fileName} /> */}
+
+            {/* <br/>
+
+            {canClick ? 
+                <Button 
+                    disabled={true} 
+                    variant="contained" 
+                    style={{ marginLeft: '38%', backgroundColor: '#79719880', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
+                    disableElevation
+                    >
+                        Submit
+                    </Button>
+
+                :
+
+                <Button 
+                    disabled={false} 
+                    variant="contained" 
+                    style={{ marginLeft: '38%', backgroundColor: '#797198', color: 'white', marginTop: 25, width: '344px', height: '44px' }} 
+                    disableElevation 
+                    component="label"
+                    onClick={resetUploadPage}
+                >
+                    Submit
+                </Button>
+            }
+
+            <br/>
+
+            {loading ? 
+                <CircularProgress style={{ color: '#797198', marginTop: 50, marginLeft: '44%', width: '150px', height: '150px'}}/>
+                :
+                null
+            }
+            {uploadComplete ?
+                <h2 style={{ color: '#797198', marginTop: 50, marginLeft: '42.5%' }}>Upload Complete</h2>
+                :
+                null
+            } */}
         </>
     )
 }
